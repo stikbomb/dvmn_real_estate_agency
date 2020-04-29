562acbf638399d85f87eebce53e27b81f2958edb
stikbomb <stikbomb@gmail.com>
2020-04-29 23:49:15 +0300

    Add field Flat.new_building, new migration

--- property/models.py
+++ property/models.py
@@ -6,7 +6,7 @@ class Flat(models.Model):
     owner = models.CharField("ФИО владельца", max_length=200)
     owners_phonenumber = models.CharField("Номер владельца", max_length=20)
     created_at = models.DateTimeField("Когда создано объявление", default=timezone.now, db_index=True)
-    
+
     description = models.TextField("Текст объявления", blank=True)
     price = models.IntegerField("Цена квартиры", db_index=True)
 
@@ -21,6 +21,7 @@ class Flat(models.Model):
     has_balcony = models.NullBooleanField("Наличие балкона", db_index=True)
     active = models.BooleanField("Активно-ли объявление", db_index=True)
     construction_year = models.IntegerField("Год постройки здания", null=True, blank=True, db_index=True)
+    new_building = models.NullBooleanField("Является ли новостройкой", blank=True, db_index=True)
 
     def __str__(self):
         return f"{self.town}, {self.address} ({self.price}р.)"
