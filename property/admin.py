bc4525963c8e35319b44cb1af4359cc1a6aed03e
stikbomb <stikbomb@gmail.com>
2020-05-07 19:34:53 +0300

    owner model view in admin fix

--- property/admin.py
+++ property/admin.py
@@ -18,6 +18,7 @@ class ComplaintAdmin(admin.ModelAdmin):
 
 
 class OwnerAdmin(admin.ModelAdmin):
+    list_display = ('name', 'phonenumber', 'pure_phonenumber')
     raw_id_fields = ('flats_in_property',)
 
 
